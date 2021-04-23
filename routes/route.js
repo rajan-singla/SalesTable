a7b24efb9d2da6c5418ca838d7d67704cfbe8b3c
RAJAN-PC\lenovo <rajanabohar@gmail.com>
2021-04-23 21:13:47 +0530

    #Updated  - route.js - convert if else to switch statement  - common_functions.js - added a functions =>  daily_data,weekly_data,monthly_data,yearly_data

--- routes/route.js
+++ routes/route.js
@@ -6,6 +6,7 @@ const bodyparser = require('body-parser');
 
 //MODAL
 const SALES_TABLE = require('./../Models/salesTableSchema');
+const common_functions = require('./../utils/common_functions');
 
 router.use(bodyparser.json());
 router.use(bodyparser.urlencoded({extended: true}));
@@ -40,190 +41,34 @@ router.post('/insertData',async (req,res)=>{
   } 
 });
 
-//api to fetch stats from the sales table
+//api to fetch stats from the sales table based on the query 
 router.get('/fetch_stats/:query',async (req,res) =>{
     console.log(req.params);
 
-    const startOfWeek = moment().startOf('week').toDate();
-    const endOfWeek = moment().endOf('week').toDate();
-    const startOfMonth = moment().clone().startOf('month').toDate();
-    const endOfMonth = moment().clone().endOf('month').toDate();
-    const startOfYear = moment().clone().startOf('year').toDate();
-    const endOfYear = moment().clone().endOf('year').toDate();
-    
-    let totalAmount = 0;
-    let totalHours = 24;
-
-    //to store the final result 
-    var data = [];
-    const today = moment().startOf('day');
-
-    //On the basis of each hour 
-    if(req.params.query == 'daily'){
-
-       await SALES_TABLE.find({
-        daily: req.body.amount,
-        date:{
-                $gte: today.toDate(),
-                $lte: moment(today).endOf('day').toDate()
-            },
-        },{
-            amount: 1,
-            date: 1,
-            _id : 0
-        },
-        (error,result) => {
-            if(error){
-                return res.json({
-                    status: false,
-                    message: 'Unable to fetch the amount on daily basis',
-                    error: error
-                });
-            }
-
-            for (let i = 0; i < totalHours; i++) {
-                totalAmount = 0;
-                //For getting the amount based on hours
-                result.forEach(element => {
-                    if(i == element.date.getHours()){
-                        totalAmount += element.amount;
-                    }
-                });
-
-                data.push({hour: i + ":00 to " + (i+1)+ ":00",amount: totalAmount});
-            }
-
-            return res.json({
-                    status: true,
-                    message: 'Amount is successfully fetched',
-                    result: data
-                });
-        });
-    }else if(req.params.query == 'weekly'){
-        await SALES_TABLE.find({
-            weekly: req.body.amount,
-            date:{ 
-                $gte: startOfWeek,
-                $lte: endOfWeek
-            },
-        },{
-            amount: 1,
-            date:1,
-            _id : 0
-        },
-        (error,result) => {
-            if(error){
-                return res.json({
-                    status: false,
-                    message: 'Unable to fetch the amount on weekly basis',
-                    error: error
-                });
-            }
-
-            
-
-            for (var m = moment(startOfWeek); m.isBefore(endOfWeek); m.add(1, 'days')) {
-                totalAmount = 0;
-                //For getting the amount based on weekdays
-                result.forEach(element => {
-                    if(m.day() == element.date.getDay()){
-                        totalAmount += element.amount;
-                    }
-                });
-
-                data.push({weekday: m.format('dddd'),amount: totalAmount});
-            }
-
-            return res.json({
-                    status: true,
-                    message: 'Amount is successfully fetched',
-                    result: data
-                });
-        });
-    }else if(req.params.query == 'monthly'){
-        await SALES_TABLE.find({
-            monthly: req.body.amount,
-            date:{ 
-                $gte: startOfMonth,
-                $lte: endOfMonth
-            },
-        },{
-            amount: 1,
-            date:1,
-            _id : 0
-        },
-        (error,result) => {
-            if(error){
-                return res.json({
-                    status: false,
-                    message: 'Unable to fetch the amount on monthly basis',
-                    error: error
-                });
-            }
-
-            for (var m = moment(startOfMonth); m.isBefore(endOfMonth); m.add(1, 'months')) {
-                totalAmount = 0;
-
-                //For getting the amount based on weekdays
-                result.forEach(element => {
-                    if(m.month() == element.date.getMonth()){
-                        totalAmount += element.amount;
-                    }
-                });
-
-                data.push({month: m.format('MMMM'),amount: totalAmount});
-            }
-            return res.json({
-                    status: true,
-                    message: 'Amount is successfully fetched',
-                    result: data
-                });
-        });
-    }else if(req.params.query == 'yearly'){
-        await SALES_TABLE.find({
-            monthly: req.body.amount,
-            date:{ 
-                $gte: startOfYear,
-                $lte: endOfYear
-            },
-        },{
-            amount: 1,
-            date:1,
-            _id : 0
-        },
-        (error,result) => {
-            if(error){
-                return res.json({
-                    status: false,
-                    message: 'Unable to fetch the amount on monthly basis',
-                    error: error
-                });
-            }
-
-            for (var m = moment(startOfYear); m.isBefore(endOfYear); m.add(1, 'months')) {
-                totalAmount = 0;
-                console.log(m.year());
-
-                //For getting the amount based on weekdays
-                result.forEach(element => {
-                    if(m.month() == element.date.getMonth()){
-                        totalAmount += element.amount;
-                    }
-                });
-
-                data.push({month: m.format('MMMM'),amount: totalAmount});
-            }
-            return res.json({
-                    status: true,
-                    message: 'Amount is successfully fetched',
-                    result: data
-                });
-        });
+    switch(req.params.query) {
+        case 'daily':
+                common_functions.dailyData(req,res);
+            break;
+        case 'weekly':
+                common_functions.weeklyData(req,res);
+            break;
+        case 'monthly':
+                common_functions.monthlyData(req,res);
+            break;
+        case 'yearly':
+                common_functions.yearlyData(req,res);
+            break;
+        default:
+            res.json({
+                status: false,
+                message: "Searching for the wrong query - Supported queries are /user/fetch_stats/daily or weekly or monthly or yearly"
+            });
+        // code block
     }
 }); 
 
 
-//To fetch all the records
+//api to fetch all the records
 router.get('/fetchAll',async (req,res) => {
 
     await SALES_TABLE.find({},(error,result)=>{
--- utils/common_functions.js
+++ utils/common_functions.js
@@ -1,3 +1,212 @@
+const moment = require('moment');
+//MODAL
+const SALES_TABLE = require('./../Models/salesTableSchema');
+
+const startOfWeek = moment().startOf('week').toDate();
+const endOfWeek = moment().endOf('week').toDate();
+const startOfMonth = moment().clone().startOf('month').toDate();
+const endOfMonth = moment().clone().endOf('month').toDate();
+const startOfYear = moment().clone().startOf('year').toDate();
+const endOfYear = moment().clone().endOf('year').toDate();
+
+//function to get the data based on hourly basis for the current
+var daily_data = (req,res)=> {
+
+    let totalAmount = 0;
+    let totalHours = 24;
+
+    //to store the final result 
+    var data = [];
+    const today = moment().startOf('day');
+
+  return SALES_TABLE.find({
+        daily: req.body.amount,
+        date:{
+                $gte: today.toDate(),
+                $lte: moment(today).endOf('day').toDate()
+            },
+        },{
+            amount: 1,
+            date: 1,
+            _id : 0
+        },
+        (error,result) => {
+            if(error){
+                return res.json({
+                    status: false,
+                    message: 'Unable to fetch the amount on daily basis',
+                    error: error
+                });
+            }
+
+            for (let i = 0; i < totalHours; i++) {
+                totalAmount = 0;
+                //For getting the amount based on hours
+                result.forEach(element => {
+                    if(i == element.date.getHours()){
+                        totalAmount += element.amount;
+                    }
+                });
+
+                data.push({hour: i + ":00 to " + (i+1)+ ":00",amount: totalAmount});
+            }
+
+            return res.json({
+                    status: true,
+                    message: 'Amount is successfully fetched',
+                    result: data
+                });
+        });
+
+}
+
+//function to get the data based on week days
+var weekly_data = (req,res)=>{
+
+    let totalAmount = 0;
+
+    //to store the final result 
+    var data = [];
+
+  return SALES_TABLE.find({
+            weekly: req.body.amount,
+            date:{ 
+                $gte: startOfWeek,
+                $lte: endOfWeek
+            },
+        },{
+            amount: 1,
+            date:1,
+            _id : 0
+        },
+        (error,result) => {
+            if(error){
+                return res.json({
+                    status: false,
+                    message: 'Unable to fetch the amount on weekly basis',
+                    error: error
+                });
+            }
+
+            
+
+            for (var m = moment(startOfWeek); m.isBefore(endOfWeek); m.add(1, 'days')) {
+                totalAmount = 0;
+                //For getting the amount based on weekdays
+                result.forEach(element => {
+                    if(m.day() == element.date.getDay()){
+                        totalAmount += element.amount;
+                    }
+                });
+
+                data.push({weekday: m.format('dddd'),amount: totalAmount});
+            }
+
+            return res.json({
+                    status: true,
+                    message: 'Amount is successfully fetched',
+                    result: data
+                });
+        });
+}
+
+//function to get the data based on the month which are present in the database
+var monthly_data = (req,res) => {
+
+    let totalAmount = 0;
+
+    //to store the final result 
+    var data = [];
+
+  return SALES_TABLE.find({
+            monthly: req.body.amount,
+            date:{ 
+                $gte: startOfMonth,
+                $lte: endOfMonth
+            },
+        },{
+            amount: 1,
+            date:1,
+            _id : 0
+        },
+        (error,result) => {
+            if(error){
+                return res.json({
+                    status: false,
+                    message: 'Unable to fetch the amount on monthly basis',
+                    error: error
+                });
+            }
+
+            for (var m = moment(startOfMonth); m.isBefore(endOfMonth); m.add(1, 'months')) {
+                totalAmount = 0;
+
+                //For getting the amount based on weekdays
+                result.forEach(element => {
+                    if(m.month() == element.date.getMonth()){
+                        totalAmount += element.amount;
+                    }
+                });
+
+                data.push({month: m.format('MMMM'),amount: totalAmount});
+            }
+            return res.json({
+                    status: true,
+                    message: 'Amount is successfully fetched',
+                    result: data
+                });
+        });
+}
+
+//Function to get the data based on each month in a year 
+var yearly_data = (req,res) =>{
+
+    let totalAmount = 0;
+
+    //to store the final result 
+    var data = [];
+
+  return SALES_TABLE.find({
+            monthly: req.body.amount,
+            date:{ 
+                $gte: startOfYear,
+                $lte: endOfYear
+            },
+        },{
+            amount: 1,
+            date:1,
+            _id : 0
+        },
+        (error,result) => {
+            if(error){
+                return res.json({
+                    status: false,
+                    message: 'Unable to fetch the amount on monthly basis',
+                    error: error
+                });
+            }
+
+            for (var m = moment(startOfYear); m.isBefore(endOfYear); m.add(1, 'months')) {
+                totalAmount = 0;
+                console.log(m.year());
+
+                //For getting the amount based on weekdays
+                result.forEach(element => {
+                    if(m.month() == element.date.getMonth()){
+                        totalAmount += element.amount;
+                    }
+                });
+
+                data.push({month: m.format('MMMM'),amount: totalAmount});
+            }
+            return res.json({
+                    status: true,
+                    message: 'Amount is successfully fetched',
+                    result: data
+                });
+        });
+}
+
 //Checking the today's date
 const isToday = (someDate) => {
   const today = new Date();
@@ -7,3 +216,7 @@ const isToday = (someDate) => {
 }
 
 exports.isToday = isToday;
+exports.dailyData = daily_data;
+exports.weeklyData = weekly_data;
+exports.monthlyData = monthly_data;
+exports.yearlyData = yearly_data;
